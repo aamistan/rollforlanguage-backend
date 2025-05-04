663a0652e2b87a390735ef08849adbd42c97af4c
aamistan <aaron.stanley@fwcs.k12.in.us>
2025-05-04 00:58:20 +0000

    import jwtPlugin in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import Fastify from 'fastify';
 import { authRoutes } from './routes/auth.route';
+import jwtPlugin from './plugins/jwt.plugin';
 
 const app = Fastify({
   logger: true,
 });
 
-// Register routes
+// Register plugins BEFORE routes
+app.register(jwtPlugin);
 app.register(authRoutes, { prefix: '/auth' });
 
 export default app;
